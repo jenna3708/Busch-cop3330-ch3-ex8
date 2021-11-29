079ca7bdc30a36db33f31b4fcf45d290161e2347
jenna3708 <53448385+jenna3708@users.noreply.github.com>
2021-11-28 23:02:07 -0500

    Add files via upload

---
+++ ch3ex8.cpp
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Assignment 5 Solution
+ *  Copyright 2021 Jenna Busch
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ int number; 
+
+ cout << "Enter an integer:";
+ cin >> number;
+
+ if(number % 2==0)
+ {
+   cout << "Your number is even!";
+ }
+ else{
+   cout << "Your number is odd!";
+ }
+
+ return 0;
+} 
